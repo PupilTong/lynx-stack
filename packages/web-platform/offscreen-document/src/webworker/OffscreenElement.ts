f7e8fa1a294a2005e7311b3997943b2af5daa6b1
pupiltong <12288479+PupilTong@users.noreply.github.com>
2025-05-28 18:15:02 +0800

    wip

--- packages/web-platform/offscreen-document/src/webworker/OffscreenElement.ts
+++ packages/web-platform/offscreen-document/src/webworker/OffscreenElement.ts
@@ -6,249 +6,225 @@ import {
   operations,
   type OffscreenDocument,
 } from './OffscreenDocument.js';
-import { OffscreenCSSStyleDeclaration } from './OffscreenCSSStyleDeclaration.js';
 import { OperationType } from '../types/ElementOperation.js';
 
 export const ancestorDocument = Symbol('ancestorDocument');
-export const _attributes = Symbol('_attributes');
-export const _children = Symbol('_children');
+export const attributes = Symbol('attributes');
+export const children = Symbol('children');
 export const innerHTML = Symbol('innerHTML');
 export const uniqueId = Symbol('uniqueId');
-const _style = Symbol('_style');
-export class OffscreenElement extends EventTarget {
-  public [innerHTML]: string = '';
-  private [_style]?: OffscreenCSSStyleDeclaration;
-  private readonly [_attributes] = new Map<string, string>();
-  private _parentElement: OffscreenElement | null = null;
-  readonly [_children]: OffscreenElement[] = [];
-  /**
-   * @private
-   */
-  readonly [uniqueId]: number;
+export const parentElement = Symbol('parentElement');
+export const eventTarget = Symbol('eventTarget');
 
-  /**
-   * @private
-   */
-  [ancestorDocument]!: OffscreenDocument;
+interface OffscreenElementData {
+  [attributes]: Map<string, string>;
+  [children]: OffscreenElementData[];
+  [ancestorDocument]: OffscreenDocument;
+  [uniqueId]: number;
+  [parentElement]: OffscreenElementData | null;
+  [innerHTML]: string;
+  [eventTarget]: EventTarget;
+  localName: string;
+}
 
-  public readonly localName: string;
+function getTagName(this: OffscreenElementData): string {
+  return this.localName.toUpperCase();
+}
 
-  constructor(
-    localName: string,
-    elementUniqueId: number,
-  ) {
-    super();
-    this.localName = localName;
-    this[uniqueId] = elementUniqueId;
-  }
+function getId(this: OffscreenElementData): string {
+  return this[attributes].get('id') ?? '';
+}
 
-  get tagName(): string {
-    return this.localName.toUpperCase();
-  }
+function setId(this: OffscreenElementData, value: string): void {
+  setAttribute.call(this, 'id', value);
+}
 
-  get style(): OffscreenCSSStyleDeclaration {
-    if (!this[_style]) {
-      this[_style] = new OffscreenCSSStyleDeclaration(
-        this,
-      );
-    }
-    return this[_style];
-  }
+function getFirstElementChild(
+  this: OffscreenElementData,
+): OffscreenElementData | null {
+  return this[children][0] ?? null;
+}
 
-  get id(): string {
-    return this[_attributes].get('id') ?? '';
-  }
+function getLastElementChild(
+  this: OffscreenElementData,
+): OffscreenElementData | null {
+  return this[children][this[children].length - 1] ?? null;
+}
 
-  set id(value: string) {
-    this[_attributes].set('id', value);
-    this.setAttribute('id', value);
-  }
-
-  get className(): string {
-    return this[_attributes].get('class') ?? '';
-  }
-
-  get children(): OffscreenElement[] {
-    return this[_children].slice();
-  }
-
-  get parentElement(): OffscreenElement | null {
-    return this._parentElement;
-  }
-
-  get parentNode(): OffscreenElement | null {
-    return this._parentElement;
-  }
-
-  get firstElementChild(): OffscreenElement | null {
-    return this[_children][0] ?? null;
-  }
-
-  get lastElementChild(): OffscreenElement | null {
-    return this[_children][this[_children].length - 1] ?? null;
-  }
-
-  get nextElementSibling(): OffscreenElement | null {
-    const parent = this._parentElement;
-    if (parent) {
-      const nextElementSiblingIndex = parent[_children].indexOf(this);
-      if (nextElementSiblingIndex >= 0) {
-        return parent[_children][nextElementSiblingIndex + 1] || null;
-      }
-    }
-    return null;
-  }
-
-  protected _remove(): void {
-    if (this._parentElement) {
-      const currentIdx = this._parentElement[_children].indexOf(this);
-      this._parentElement[_children].splice(currentIdx, 1);
-      this._parentElement = null;
+function getNextElementSibling(
+  this: OffscreenElementData,
+): OffscreenElementData | null {
+  const parent = this[parentElement];
+  if (parent) {
+    const nextElementSiblingIndex = parent[children].indexOf(this);
+    if (nextElementSiblingIndex >= 0) {
+      return parent[children][nextElementSiblingIndex + 1] || null;
     }
   }
+  return null;
+}
 
-  setAttribute(qualifiedName: string, value: string): void {
-    this[_attributes].set(qualifiedName, value);
-    this[ancestorDocument][operations].push({
-      type: OperationType.SetAttribute,
-      uid: this[uniqueId],
-      key: qualifiedName,
-      value,
-    });
+function setAttribute(
+  this: OffscreenElementData,
+  qualifiedName: string,
+  value: string,
+) {
+  this[attributes].set(qualifiedName, value);
+  this[ancestorDocument][operations].push({
+    type: OperationType.SetAttribute,
+    uid: this[uniqueId],
+    key: qualifiedName,
+    value,
+  });
+}
+
+function getAttribute(
+  this: OffscreenElementData,
+  qualifiedName: string,
+): string | null {
+  return this[attributes].get(qualifiedName) ?? null;
+}
+
+function removeAttribute(
+  this: OffscreenElementData,
+  qualifiedName: string,
+): void {
+  this[attributes].delete(qualifiedName);
+  this[ancestorDocument][operations].push({
+    type: OperationType.RemoveAttribute,
+    uid: this[uniqueId],
+    key: qualifiedName,
+  });
+}
+
+function _remove(element: OffscreenElementData): void {
+  if (element[parentElement]) {
+    const currentIdx = element[parentElement][children].indexOf(element);
+    element[parentElement][children].splice(currentIdx, 1);
+    element[parentElement] = null;
   }
-
-  getAttribute(qualifiedName: string): string | null {
-    return this[_attributes].get(qualifiedName) ?? null;
+}
+function append(
+  this: OffscreenElementData,
+  ...nodes: OffscreenElementData[]
+): void {
+  this[ancestorDocument][operations].push({
+    type: OperationType.Append,
+    uid: this[uniqueId],
+    cid: nodes.map(node => node[uniqueId]),
+  });
+  for (const node of nodes) {
+    _remove(node);
+    node[parentElement] = this;
   }
-
-  removeAttribute(qualifiedName: string): void {
-    this[_attributes].delete(qualifiedName);
-    this[ancestorDocument][operations].push({
-      type: OperationType.RemoveAttribute,
-      uid: this[uniqueId],
-      key: qualifiedName,
-    });
-  }
-
-  append(...nodes: (OffscreenElement)[]): void {
-    this[ancestorDocument][operations].push({
-      type: OperationType.Append,
-      uid: this[uniqueId],
-      cid: nodes.map(node => node[uniqueId]),
-    });
+  this[children].push(...nodes);
+}
+function replaceWith(
+  this: OffscreenElementData,
+  ...nodes: (OffscreenElementData)[]
+): void {
+  this[ancestorDocument][operations].push({
+    type: OperationType.ReplaceWith,
+    uid: this[uniqueId],
+    nid: nodes.map(node => node[uniqueId]),
+  });
+  if (this[parentElement]) {
+    const parent = this[parentElement];
+    this[parentElement] = null;
+    const currentIdx = parent[children].indexOf(this);
+    parent[children].splice(currentIdx, 1, ...nodes);
     for (const node of nodes) {
-      node._remove();
-      node._parentElement = this;
-    }
-    this[_children].push(...nodes);
-  }
-
-  appendChild(node: OffscreenElement): OffscreenElement {
-    this[ancestorDocument][operations].push({
-      type: OperationType.Append,
-      uid: this[uniqueId],
-      cid: [node[uniqueId]],
-    });
-    node._remove();
-    node._parentElement = this;
-    this[_children].push(node);
-    return node;
-  }
-
-  replaceWith(...nodes: (OffscreenElement)[]): void {
-    this[ancestorDocument][operations].push({
-      type: OperationType.ReplaceWith,
-      uid: this[uniqueId],
-      nid: nodes.map(node => node[uniqueId]),
-    });
-    if (this._parentElement) {
-      const parent = this._parentElement;
-      this._parentElement = null;
-      const currentIdx = parent[_children].indexOf(this);
-      parent[_children].splice(currentIdx, 1, ...nodes);
-      for (const node of nodes) {
-        node._parentElement = parent;
-      }
+      node[parentElement] = parent;
     }
   }
+}
 
-  getAttributeNames(): string[] {
-    return [...this[_attributes].keys()];
-  }
+function getAttributeNames(this: OffscreenElementData): string[] {
+  return [...this[attributes].keys()];
+}
 
-  remove(): void {
-    this[ancestorDocument][operations].push({
-      type: OperationType.Remove,
-      uid: this[uniqueId],
-    });
-    this._remove();
-  }
+function remove(this: OffscreenElementData): void {
+  this[ancestorDocument][operations].push({
+    type: OperationType.Remove,
+    uid: this[uniqueId],
+  });
+  _remove(this);
+}
 
-  insertBefore(
-    newNode: OffscreenElement,
-    refNode: OffscreenElement | null,
-  ): OffscreenElement {
-    newNode._remove();
-    if (refNode) {
-      const refNodeIndex = this[_children].indexOf(refNode);
-      if (refNodeIndex >= 0) {
-        newNode._parentElement = this;
-        this[_children].splice(refNodeIndex, 0, newNode);
-      }
-    } else {
-      newNode._parentElement = this;
-      this[_children].push(newNode);
+function insertBefore(
+  this: OffscreenElementData,
+  newNode: OffscreenElementData,
+  refNode: OffscreenElementData | null,
+): OffscreenElementData {
+  _remove(newNode);
+  if (refNode) {
+    const refNodeIndex = this[children].indexOf(refNode);
+    if (refNodeIndex >= 0) {
+      newNode[parentElement] = this;
+      this[children].splice(refNodeIndex, 0, newNode);
     }
-
-    this[ancestorDocument][operations].push({
-      type: OperationType.InsertBefore,
-      uid: this[uniqueId],
-      cid: newNode[uniqueId],
-      ref: refNode?.[uniqueId],
-    });
-    return newNode;
+  } else {
+    newNode[parentElement] = this;
+    this[children].push(newNode);
   }
 
-  removeChild(child: OffscreenElement | null): OffscreenElement {
-    if (!child) {
-      throw new DOMException(
-        'The node to be removed is not a child of this node.',
-        'NotFoundError',
-      );
-    }
-    if (child._parentElement !== this) {
-      throw new DOMException(
-        'The node to be removed is not a child of this node.',
-        'NotFoundError',
-      );
-    }
-    this[ancestorDocument][operations].push({
-      type: OperationType.RemoveChild,
-      uid: this[uniqueId],
-      cid: child![uniqueId],
-    });
-    child._remove();
-    return child;
-  }
+  this[ancestorDocument][operations].push({
+    type: OperationType.InsertBefore,
+    uid: this[uniqueId],
+    cid: newNode[uniqueId],
+    ref: refNode?.[uniqueId],
+  });
+  return newNode;
+}
 
-  override addEventListener(
-    type: string,
-    callback: EventListenerOrEventListenerObject | null,
-    options?: AddEventListenerOptions | boolean,
-  ): void {
-    this[ancestorDocument][enableEvent](type, this[uniqueId]);
-    super.addEventListener(type, callback, options);
+function removeChild(
+  this: OffscreenElementData,
+  child: OffscreenElementData | null,
+): OffscreenElementData {
+  if (!child) {
+    throw new DOMException(
+      'The node to be removed is not a child of this node.',
+      'NotFoundError',
+    );
   }
+  if (child[parentElement] !== this) {
+    throw new DOMException(
+      'The node to be removed is not a child of this node.',
+      'NotFoundError',
+    );
+  }
+  this[ancestorDocument][operations].push({
+    type: OperationType.RemoveChild,
+    uid: this[uniqueId],
+    cid: child![uniqueId],
+  });
+  _remove(child);
+  return child;
+}
 
-  set innerHTML(text: string) {
-    this[ancestorDocument][operations].push({
-      type: OperationType.SetInnerHTML,
-      text,
-      uid: this[uniqueId],
-    });
-    for (const child of this.children) {
-      (child as OffscreenElement).remove();
-    }
-    this[innerHTML] = text;
-  }
+function addEventListener(
+  this: OffscreenElementData,
+  type: string,
+  callback: EventListenerOrEventListenerObject | null,
+  options?: AddEventListenerOptions | boolean,
+): void {
+  this[ancestorDocument][enableEvent](type, this[uniqueId]);
+  this[eventTarget].addEventListener(type, callback, options);
+}
+
+export function createOffscreenElement(
+  localName: string,
+  elementUniqueId: number,
+  ancestor: OffscreenDocument,
+) {
+  const element: OffscreenElementData = {
+    [attributes]: new Map(),
+    [children]: [],
+    [ancestorDocument]: ancestor,
+    [uniqueId]: elementUniqueId,
+    [parentElement]: null,
+    [innerHTML]: '',
+    [eventTarget]: new EventTarget(),
+    localName,
+  };
 }
