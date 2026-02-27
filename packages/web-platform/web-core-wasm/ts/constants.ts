181edd8b0259d1333b9d628d4c96ee3028992859
pupiltong <12288479+PupilTong@users.noreply.github.com>
2026-02-27 19:13:00 +0800

    +fix

--- packages/web-platform/web-core-wasm/ts/constants.ts
+++ packages/web-platform/web-core-wasm/ts/constants.ts
@@ -48,7 +48,7 @@ export const LynxEventNameToW3cCommon: Record<string, string> =
     Object.entries(W3cEventNameToLynx).map(([k, v]) => [v, k]),
   );
 
-export const MagicHeader = /*#__PURE__*/ 0x464F525741524453; // random magic number for verifying the stream is a Lynx encoded template
+export const MagicHeader = /*#__PURE__*/ 0x464F525741524453n; // random magic number for verifying the stream is a Lynx encoded template
 
 export const TemplateSectionLabel = /*#__PURE__*/ {
   Manifest: 1,
